5b61adef6065016ba92a9e40491d9bca2b219139
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-11-29 16:05:10 +0900

    Board BoardController BoardRepository BoardService boardlist boardwrite

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,15 +1,45 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
-        return "Hello world";
-
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String main(){
+//        return "Hello world";
+//
+//    }
+    @Autowired
+    private BoardService boardService;
+    @GetMapping("/board/write")//어떤 url로 접근할 것인지 지정 localhost:8080/board/write
+    public String boardWriteForm(){
+        //어떤 viewfile인지 입력
+        return "boardwrite";
     }
+//    @RequestParam("title") String title, @RequestParam("content") String content
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+//        System.out.println("제목 : "+title);
+//        System.out.println("내용 : "+content);
+//        System.out.println(board.getTitle());
+        boardService.write(board);
+
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(){
+
+        return "boardlist";
+    }
+
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,22 @@
+package com.study.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//Entity는 class가 DB에 있는 table이랑 같다고 해주는거임
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+
+
+
+
+}
